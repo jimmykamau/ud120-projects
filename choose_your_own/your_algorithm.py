8fd2a94f46ef3217f8559d6d9bab6a95080fbfe6
Jimmy Kamau <jimmy.kamau@andela.com>
2017-10-14 18:08:24 +0300

    Use KNN for 0.944 accuracy

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-
+from time import time
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
@@ -7,36 +9,44 @@ from class_vis import prettyPicture
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
-### the training data (features_train, labels_train) have both "fast" and "slow"
-### points mixed together--separate them so we can give them different colors
-### in the scatterplot and identify them visually
-grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+# the training data (features_train, labels_train) have both "fast" and "slow"
+# points mixed together--separate them so we can give them different colors
+# in the scatterplot and identify them visually
+grade_fast = [features_train[ii][0]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+bumpy_fast = [features_train[ii][1]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+grade_slow = [features_train[ii][0]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 1]
+bumpy_slow = [features_train[ii][1]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 1]
 
 
-#### initial visualization
+# initial visualization
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.scatter(bumpy_fast, grade_fast, color="b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color="r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
 plt.show()
-################################################################################
+##############################################################
 
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+# your code here!  name your classifier object clf if you want the
+# visualization code (prettyPicture) to show you the decision boundary
+clf = KNeighborsClassifier(n_neighbors=22)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: {}s".format(round(time() - t0, 3))
 
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time: {}s".format(round(time() - t0, 3))
 
-
-
-
-
+print accuracy_score(pred, labels_test)
 
 try:
     prettyPicture(clf, features_test, labels_test)
