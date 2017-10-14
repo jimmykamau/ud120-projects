4d78503e1bb218b1bb7f48ba36c41e6d44b062d6
Jimmy Kamau <jimmy.kamau@andela.com>
2017-10-14 19:48:55 +0300

    Explore Enron data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,16 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
 """
-
+from collections import Counter
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+enron_data = pickle.load(
+    open("../final_project/final_project_dataset.pkl", "r"))
 
-
+number_of_pois = len(
+    Counter(person for person in enron_data if enron_data[person]["poi"]))
+print len(
+    Counter(person for person in enron_data
+            if enron_data[person]["total_payments"] == "NaN"))
+print number_of_pois
