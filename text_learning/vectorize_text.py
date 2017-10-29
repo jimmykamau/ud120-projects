0e1b9c93d043f660d402097b07f6e266e6977ba5
Jimmy Kamau <jimmy.kamau@andela.com>
2017-10-29 19:57:41 +0300

    Complete text learning task

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -5,7 +5,8 @@ import pickle
 import re
 import sys
 
-sys.path.append( "../tools/" )
+from sklearn.feature_extraction.text import TfidfVectorizer
+sys.path.append("../tools/")
 from parse_out_email_text import parseOutText
 
 """
@@ -23,53 +24,53 @@ from parse_out_email_text import parseOutText
 """
 
 
-from_sara  = open("from_sara.txt", "r")
+from_sara = open("from_sara.txt", "r")
 from_chris = open("from_chris.txt", "r")
 
 from_data = []
 word_data = []
 
-### temp_counter is a way to speed up the development--there are
-### thousands of emails from Sara and Chris, so running over all of them
-### can take a long time
-### temp_counter helps you only look at the first 200 emails in the list so you
-### can iterate your modifications quicker
+# temp_counter is a way to speed up the development--there are
+# thousands of emails from Sara and Chris, so running over all of them
+# can take a long time
+# temp_counter helps you only look at the first 200 emails in the list so you
+# can iterate your modifications quicker
 temp_counter = 0
 
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
-        ### only look at first 200 emails when developing
-        ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # only look at first 200 emails when developing
+        # once everything is working, remove this line to run over full dataset
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            # print path
             email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
-
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
-
-            ### append the text to word_data
-
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
+            # use parseOutText to extract the text from the opened email
+            parsed_email = parseOutText(email)
+            # use str.replace() to remove any instances of the words
+            for word in ["sara", "shackleton", "chris", "germani"]:
+                parsed_email = parsed_email.replace(word, "")
+            # append the text to word_data
+            word_data.append(parsed_email)
+            # append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            from_data.append(0 if name == "sara" else 1)
             email.close()
 
 print "emails processed"
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump(word_data, open("your_word_data.pkl", "w"))
+pickle.dump(from_data, open("your_email_authors.pkl", "w"))
 
+# print(word_data[152])
 
-
-
-
-### in Part 4, do TfIdf vectorization here
-
-
+# in Part 4, do TfIdf vectorization here
+tfidf_vectorizer = TfidfVectorizer(stop_words="english")
+tfidf_vectorizer.fit_transform(word_data)
+feature_names = tfidf_vectorizer.get_feature_names()
+print(len(feature_names))
+print(feature_names[34597])
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -3,44 +3,43 @@
 from nltk.stem.snowball import SnowballStemmer
 import string
 
+
 def parseOutText(f):
     """ given an opened email file f, parse out all text below the
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
         in the email (space-separated) 
-        
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
-
-    f.seek(0)  ### go back to beginning of file (annoying)
+    f.seek(0)  # go back to beginning of file (annoying)
     all_text = f.read()
 
-    ### split off metadata
+    # split off metadata
     content = all_text.split("X-FileName:")
     words = ""
     if len(content) > 1:
-        ### remove punctuation
-        text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
+        # remove punctuation
+        text_string = content[1].translate(
+            string.maketrans("", ""), string.punctuation)
 
-        ### project part 2: comment out the line below
-        words = text_string
+        # project part 2: comment out the line below
+        # words = text_string
 
-        ### split the text string into individual words, stem each word,
-        ### and append the stemmed word to words (make sure there's a single
-        ### space between each stemmed word)
-        
-
-
-
-
+        # split the text string into individual words, stem each word,
+        # and append the stemmed word to words (make sure there's a single
+        # space between each stemmed word)
+        stemmer = SnowballStemmer("english")
+        text_list = text_string.split()
+        stemmed_text = [stemmer.stem(text) for text in text_list]
+        words = ' '.join(stemmed_text)
     return words
 
-    
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
@@ -48,7 +47,5 @@ def main():
     print text
 
 
-
 if __name__ == '__main__':
     main()
-
