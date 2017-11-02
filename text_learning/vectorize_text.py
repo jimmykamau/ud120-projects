ddea1588668a83a67630bc13d7847837ef3275b4
Jimmy Kamau <jimmy.kamau@andela.com>
2017-11-02 21:09:17 +0300

    Complete PCA task

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -51,7 +51,7 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             # use parseOutText to extract the text from the opened email
             parsed_email = parseOutText(email)
             # use str.replace() to remove any instances of the words
-            for word in ["sara", "shackleton", "chris", "germani"]:
+            for word in ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]:
                 parsed_email = parsed_email.replace(word, "")
             # append the text to word_data
             word_data.append(parsed_email)
