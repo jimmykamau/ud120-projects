62e4ab375c17a72bad4b5152bbad960138756c6a
Jimmy Kamau <jimmy.kamau@andela.com>
2017-11-02 22:19:40 +0300

    Complete validation task

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,21 +12,27 @@
 
 import pickle
 import sys
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(
+    open("../final_project/final_project_dataset.pkl", "r"))
 
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
+# first element is our labels, any added elements are predictor
+# features. Keep this the same for the mini-project, but you'll
+# have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
 
-
-
-### it's all yours from here forward!  
-
-
+# it's all yours from here forward!
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print accuracy_score(pred, labels_test)
